e3b37e636b4ccbdea1c93e2f57b9fcb31e63c7aa
Alex Capstick <alexcapstick97@gmail.com>
2022-09-15 10:32:16 +0100

    Fixing RTD issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("..."))
 from dcarte_transform import __version__, __description__, __author__, __title__, __copyright__
 
 project = __title__
