ac04eec335a404d5f451e453814efc332fbca8ce
Alex Capstick <alexcapstick97@gmail.com>
2022-09-15 12:43:10 +0100

    Update __init__.py

--- dcarte_transform/__init__.py
+++ dcarte_transform/__init__.py
@@ -1,6 +1,6 @@
 from .__version__ import __version__, __author__, __doc__, __title__, __copyright__
 
-from .label import Labeller
+from .label import *
 from .model_selection import *
 from .recipe import *
 from .transform import *
