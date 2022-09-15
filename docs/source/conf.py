2919037da037ac260cf0e081cf6269a26aa17431
Alex Capstick <alexcapstick97@gmail.com>
2022-09-15 17:03:04 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,5 +66,12 @@ def remove_default_value(app, what, name, obj, options, signature, return_annota
 
     return (signature, return_annotation)
 
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
 def setup(app):
     app.connect("autodoc-process-signature", remove_default_value)
+    app.connect("autodoc-skip-member", skip)
+
